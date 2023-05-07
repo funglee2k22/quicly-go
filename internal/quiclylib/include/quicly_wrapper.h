a5f21989959545e23371752c20f98639fe60204a
Vittorio Parrella <vittorioparrella+1@gmail.com>
2023-05-07 12:04:40 +0200

    correct compilation

---
+++ internal/quiclylib/include/quicly_wrapper.h
@@ -0,0 +1,33 @@
+
+#ifndef QUICLY_WRAPPER
+#define QUICLY_WRAPPER
+
+#ifdef WIN32
+
+// Including SDKDDKVer.h defines the highest available Windows platform.
+// If you wish to build your application for a previous Windows platform, include WinSDKVer.h and
+// set the _WIN32_WINNT macro to the platform you wish to support before including SDKDDKVer.h.
+#include <SDKDDKVer.h>
+
+#define WIN32_LEAN_AND_MEAN // Exclude rarely-used stuff from Windows headers
+// Windows Header Files:
+#include <windows.h>
+
+#endif // WIN32
+
+#include "quicly.h"
+#include "quicly/defaults.h"
+#include "picotls/openssl.h"
+
+enum {
+  QUICLY_OK = 0,  //!< No issue
+  QUICLY_ERROR_NOTINITILIZED = 1,  //!< InitializeWinDivertEngine was not called previously
+  QUICLY_ERROR_ALREADY_INIT  = 2,  //!< InitializeWinDivertEngine called again before CloseWinDivertEngine
+  QUICLY_ERROR_FAILED = 3,         //!< Operation failed
+  QUICLY_ERROR_NOT_OPEN = 4,       //!< Connection is not open so no state available
+};
+
+extern int  InitializeQuiclyEngine();
+extern int  CloseQuiclyEngine();
+
+#endif
--- internal/quiclylib/quiclylib_windows.go
+++ internal/quiclylib/quiclylib_windows.go
@@ -3,7 +3,7 @@
 package quiclylib
 
 //#cgo windows CPPFLAGS: -DWIN32 -D_WIN32_WINNT=0x0A00 -I${SRCDIR}/include/ -I${SRCDIR}/deps/
-//#cgo windows,amd64 LDFLAGS: -L ${SRCDIR}/lib/VC/static ${SRCDIR}/lib/libquicly.a ${SRCDIR}/lib/libpicotls.a -llibcrypto64MD -lm -lmswsock -lws2_32
+//#cgo windows,amd64 LDFLAGS: ${SRCDIR}/lib/libquicly.a ${SRCDIR}/lib/libpicotls.a ${SRCDIR}/lib/libcrypto.a ${SRCDIR}/lib/libssl.a -lm -lmswsock -lws2_32
 //#include "quicly_wrapper.h"
 import "C"
 
