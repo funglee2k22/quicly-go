c290ac96747064b128bcc10307c8dc4a455838e4
Vittorio Parrella <vittorioparrella+1@gmail.com>
2023-06-30 00:58:57 +0200

    fix stream read/write operations

--- quiclylib/stream.go
+++ quiclylib/stream.go
@@ -2,9 +2,10 @@ package quiclylib
 
 import "C"
 import (
+	"errors"
 	"fmt"
 	"github.com/Project-Faster/quicly-go/internal/bindings"
-	"github.com/Project-Faster/quicly-go/quiclylib/errors"
+	qerrors "github.com/Project-Faster/quicly-go/quiclylib/errors"
 	"github.com/Project-Faster/quicly-go/quiclylib/types"
 	"io"
 	"net"
@@ -16,42 +17,85 @@ type QStream struct {
 	id      uint64
 	session types.Session
 	conn    net.Conn
+	closed  bool
 
 	packetInQueue []packet
 	inQueueLock   sync.Mutex
+
+	readDeadline  time.Time
+	writeDeadline time.Time
 }
+
+var zeroTime = time.Time{}
+
+type timeoutErrorType struct{}
+
+func (e *timeoutErrorType) Error() string {
+	return "stream timed-out"
+}
+
+func (e *timeoutErrorType) Timeout() bool {
+	return true
+}
+
+var timeoutError = &timeoutErrorType{}
 
 func (s *QStream) ID() uint64 {
 	return s.id
 }
 
 func (s *QStream) Read(b []byte) (n int, err error) {
-	s.inQueueLock.Lock()
-	defer s.inQueueLock.Unlock()
-
-	if len(s.packetInQueue) > 0 {
-		data, lenData := s.packetInQueue[0].data, s.packetInQueue[0].dataLen
-		s.packetInQueue = s.packetInQueue[1:]
-		copy(b, data)
-		return lenData, nil
+	if s.closed {
+		return 0, io.EOF
 	}
-	return 0, io.EOF
+	deadline := s.readDeadline
+	s.readDeadline = zeroTime
+
+	first := true
+	for first || time.Now().After(deadline) {
+		first = false
+		s.inQueueLock.Lock()
+		if len(s.packetInQueue) > 0 {
+			data, lenData := s.packetInQueue[0].data, s.packetInQueue[0].dataLen
+			s.packetInQueue = s.packetInQueue[1:]
+			s.inQueueLock.Unlock()
+
+			copy(b, data)
+			return lenData, nil
+		}
+		s.inQueueLock.Unlock()
+		time.After(1 * time.Millisecond)
+	}
+	return 0, timeoutError
 }
 
 func (s *QStream) Write(b []byte) (n int, err error) {
-	if err := bindings.QuiclyWriteStream(bindings.Size_t(s.session.ID()), bindings.Size_t(s.id), b, bindings.Size_t(len(b))); err == errors.QUICLY_OK {
-		return len(b), nil
+	if s.closed {
+		return 0, io.EOF
 	}
-	return -1, io.EOF
+	deadline := s.writeDeadline
+	s.writeDeadline = zeroTime
+
+	errcode := bindings.QuiclyWriteStream(bindings.Size_t(s.session.ID()), bindings.Size_t(s.id), b, bindings.Size_t(len(b)))
+	if errcode != qerrors.QUICLY_OK {
+		return 0, errors.New(fmt.Sprintf("quicly errorcode: %d", errcode))
+	}
+	if !deadline.IsZero() && time.Now().After(deadline) {
+		return len(b), timeoutError
+	}
+	return len(b), nil
 }
 
 func (s *QStream) Close() error {
 	bindings.QuiclyCloseStream(bindings.Size_t(s.session.ID()), bindings.Size_t(s.id), int32(0))
+	s.closed = true
 	return nil
 }
 
 func (s *QStream) OnReceived(data []byte, dataLen int) {
-	fmt.Println(">>> received")
+	if s.closed {
+		return
+	}
 	if s.packetInQueue == nil {
 		s.packetInQueue = make([]packet, 0, 128)
 	}
@@ -83,24 +127,19 @@ func (s *QStream) RemoteAddr() net.Addr {
 }
 
 func (s *QStream) SetDeadline(t time.Time) error {
-	if s.conn != nil {
-		return nil
-	}
-	return s.conn.SetDeadline(t)
+	s.readDeadline = t
+	s.writeDeadline = t
+	return nil
 }
 
 func (s *QStream) SetReadDeadline(t time.Time) error {
-	if s.conn != nil {
-		return nil
-	}
-	return s.conn.SetReadDeadline(t)
+	s.readDeadline = t
+	return nil
 }
 
 func (s *QStream) SetWriteDeadline(t time.Time) error {
-	if s.conn != nil {
-		return nil
-	}
-	return s.conn.SetWriteDeadline(t)
+	s.writeDeadline = t
+	return nil
 }
 
 var _ net.Conn = &QStream{}
