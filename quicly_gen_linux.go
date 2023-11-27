990e8beed98a2b78de4b5aa67b150bcbc4ce849c
Vittorio Parrella <vittorioparrella+1@gmail.com>
2023-11-27 18:01:58 +0100

    fix auto dependency update

--- quicly_gen_linux.go
+++ quicly_gen_linux.go
@@ -1,5 +1,5 @@
 //go:generate bash -x deps_build.sh
 //go:generate bash -x regen.sh
-//go:generate cmd /c "cd tester/ && rm -f ./tester && go build -v -o tester"
+//go:generate bash -c "cd tester/ && rm -f ./tester && go build -v -o tester"
 
 package quicly
