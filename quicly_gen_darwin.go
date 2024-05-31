a83511bc464d2b67c0e9f985dc0692568a9341c1
Vittorio Parrella <vittorioparrella+1@gmail.com>
2024-05-31 23:43:33 +0200

    support darwin code generation

--- quicly_gen_darwin.go
+++ quicly_gen_darwin.go
@@ -1,5 +1,5 @@
-//go:generate zsh deps_build.sh
-//go:generate zsh regen.sh
-//go:generate zsh -c "cd tester/ && rm -f ./tester && go build -v -o tester"
+//go:generate bash deps_build.sh
+//go:generate bash regen.sh
+//go:generate bash -c "cd tester/ && rm -f ./tester && go build -v -o tester"
 
 package quicly
